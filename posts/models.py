3c76bd0db1372ba2601f482b4b935bab70b86d80
showrya619 <showrya619@gmail.com>
2021-08-21 13:27:40 +0530

    rest framework

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+# Class Post - title(Char), url(URL), poster(FK, User), created(datetime), class Meta(ordering)
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    url = models.URLField()
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+
+class Vote(models.Model):
+    voter = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
